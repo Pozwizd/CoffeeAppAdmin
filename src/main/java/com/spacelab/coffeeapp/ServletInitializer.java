8be825d4518b71de2fc7c4b9c71d0d49758e790b
Belwerk <glyuchnoglaz21@gmail.com>
2024-09-27 16:31:45 +0300

    Try deploy #2

---
+++ src/main/java/com/spacelab/coffeeapp/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.spacelab.coffeeapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoffeeAppApplication.class);
+    }
+
+}
